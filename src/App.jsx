9e63ad465fa2f9304a3da145e8524cee7de0b202
anjakely18 <anjaniaina03@gmail.com>
2024-06-20 17:16:53 +0200

    Local storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,23 @@ import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+
+  return list;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,14 +27,15 @@ const App = () => {
       property: false,
       id: nanoid(),
     };
-
-    setItems([...items, newItem]);
-    console.log(items);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   return (
