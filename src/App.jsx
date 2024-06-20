f81a51633ce4ff86f57819ebd583128a82019df5
anjakely18 <anjaniaina03@gmail.com>
2024-06-20 16:40:39 +0200

    remove item

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
+import Items from "./Items";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -15,9 +16,16 @@ const App = () => {
     setItems([...items, newItem]);
     console.log(items);
   };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+  };
+
   return (
     <section className="section-center">
       <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
     </section>
   );
 };
