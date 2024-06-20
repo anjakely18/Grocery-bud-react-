c84cb96efe3856e2ee6d87a0f8c62f9596b66d7d
anjakely18 <anjaniaina03@gmail.com>
2024-06-20 17:27:10 +0200

    edit item globally

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 
-const SingleItem = ({ item, removeItem }) => {
-  const [isChecked, setIsChecked] = useState(item.completed);
-
+const SingleItem = ({ item, removeItem, editItem }) => {
   return (
     <div className="single-item">
       <input
         type="checkbox"
-        checked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
+        checked={item.property}
+        onChange={() => editItem(item.id)}
       />
       <p
         style={{
           textTransform: "capitalize",
-          textDecoration: isChecked ? "line-through" : "none",
+          textDecoration: item.property && "line-through",
         }}
       >
         {item.name}
