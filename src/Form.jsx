e89f66bf578fd0afb4cd0989f010dd1cce66aca2
anjakely18 <anjaniaina03@gmail.com>
2024-07-22 11:26:24 +0200

    last commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
-  return ();
+const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItemName) {
+      return;
+    }
+    addItem(newItemName);
+    setNewItemName("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>grocery bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(event) => {
+            setNewItemName(event.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          Add item
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
